258320a2588ba01d2191bf2d5d48b1b07bcb0eb1
zonaetmunna <zonaetmonna@gmail.com>
2021-09-02 01:41:40 +0600

    add

--- js/book.js
+++ js/book.js
@@ -1,8 +1,19 @@
+// onclick searchBook()
 const searchBook = () => {
      const searchInput = document.getElementById("searchInput");
      const searchText = searchInput.value;
-
+     // clear search input
+     searchInput.value = '';
      // 
+     if (searchText === '') {
+
+
+          bookContainer.innerHTML = `
+           <h2 class="text-center">Something went wrong</h2>`;
+          return;
+
+     }
+     // data fetch
      const url = `http://openlibrary.org/search.json?q=${searchText}`;
      fetch(url)
           .then(res => res.json())
@@ -13,21 +24,24 @@ const displaySearchBook = books => {
      // console.log(books);
      const bookContainer = document.getElementById("bookContainer");
 
+     bookContainer.innerHTML = '';
+
      books.forEach(book => {
           console.log(book);
           const div = document.createElement("div");
           div.classList.add("col");
           div.innerHTML = `
-          <div class="card">
-                         <img src="..." class="card-img-top" alt="...">
-                         <div class="card-body">
-                              <h5 class="card-title">Card title</h5>
-                              <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                   to
-                                   additional content. This content is a little bit longer.</p>
-                         </div>
+               <div class="card h-100 border border-2 border-light p-2 bg-light rounded shadow-lg">
+                    <img height="300" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top rounded " alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title fw-bolder">${book.text[1]} ${book.text[2]}</h5>
+                         <p class="card-text">Author : ${book.author_name[0]}</p>
+                          <p class="card-text">Publish Year : ${book.first_publish_year}</p>
+                          <p class="card-text">Publish : ${book.publisher[0]}</p>
+
                     </div>
-          `
+               </div > 
+                        `
           bookContainer.appendChild(div);
 
      });
